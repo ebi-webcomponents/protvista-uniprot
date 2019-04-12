2785ac4f9190cdde20364bce4702048ae75138fa
Xavier Watkins <xavier.watkins@gmail.com>
2019-04-12 14:22:17 +0100

    Update build

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const webpack = require("webpack");
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+const PACKAGE_ROOT_PATH = process.cwd();
+const PKG_JSON = require(path.join(PACKAGE_ROOT_PATH, "package.json"));
+
+const config = {
+  entry: ["./src/index.js"],
+  output: {
+    path: path.resolve(PACKAGE_ROOT_PATH, "dist"),
+    library: "ProtvistaUniprot",
+    filename: "protvista-uniprot.js"
+  },
+  target: "web",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js"]
+  },
+  externals: {
+    "uniprot-entry-data-adapter": "UniProtEntryDataAdapter",
+    "protvista-uniprot-entry-adapter": "ProtvistaUniprotEntryAdapter",
+    d3: "d3",
+    "protvista-zoomable": "ProtvistaZoomable",
+    "protvista-track": "ProtvistaTrack"
+  },
+  plugins: [new CleanWebpackPlugin([path.join(PACKAGE_ROOT_PATH, "dist")])],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } }
+        ]
+      },
+      {
+        test: /\.(js)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            babelrc: false,
+            include: ["src", "../../node_modules/lit-html"],
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    ie: 11,
+                    browsers: "last 2 versions"
+                  },
+                  modules: false
+                }
+              ]
+            ],
+            plugins: [
+              [
+                "@babel/plugin-transform-runtime",
+                {
+                  regenerator: true
+                }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
