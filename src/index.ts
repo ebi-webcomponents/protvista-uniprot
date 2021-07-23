e3f55de52cfb24717368364c3658ba2cb5bc79f4
Xavier Watkins <xavier.watkins@gmail.com>
2021-07-23 12:56:50 +0100

    Export in cjs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { loadComponent } from './loadComponents';
 
 import ProtvistaUniprot from './protvista-uniprot';
-import DownloadPanel from './download-panel';
-import ProtvistaUniprotStructure from './protvista-uniprot-structure';
+import _DownloadPanel from './download-panel';
+import _ProtvistaUniprotStructure from './protvista-uniprot-structure';
 
 import { transformDataFeatureAdapter as _transformDataFeatureAdapter } from './protvista-uniprot';
 import { transformDataProteomicsAdapter as _transformDataProteomicsAdapter } from './protvista-uniprot';
@@ -15,9 +15,11 @@ export const transformDataProteomicsAdapter = _transformDataProteomicsAdapter;
 export const transformDataStructureAdapter = _transformDataStructureAdapter;
 export const transformDataVariationAdapter = _transformDataVariationAdapter;
 export const transformDataInterproAdapter = _transformDataInterproAdapter;
+export const ProtvistaUniprotStructure = _ProtvistaUniprotStructure;
+export const DownloadPanel = _DownloadPanel;
 
 loadComponent('protvista-uniprot', ProtvistaUniprot);
-loadComponent('download-panel', DownloadPanel);
-loadComponent('protvista-uniprot-structure', ProtvistaUniprotStructure);
+loadComponent('download-panel', _DownloadPanel);
+loadComponent('protvista-uniprot-structure', _ProtvistaUniprotStructure);
 
 export default ProtvistaUniprot;
