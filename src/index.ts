7778f0e77695ffcf3dc6719403d2a4d7ad68bb23
Daniel Rice <dlrice@ebi.ac.uk>
2024-11-11 21:14:46 +0000

    Change index exporting way to make it work.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,7 @@
+export { default as filterConfig, colorConfig } from './filter-config';
+export { default as getFeatureTooltip } from './tooltips/featureTooltip';
+export { default as getStructureTooltip } from './tooltips/structureTooltip';
+export { default as getVariationTooltip } from './tooltips/variationTooltip';
+export { default as ProtvistaUniprotStructure } from './protvista-uniprot-structure';
 import ProtvistaUniprot from './protvista-uniprot';
-import ProtvistaUniprotStructure from './protvista-uniprot-structure';
-
-import filterConfig, { colorConfig } from './filter-config';
-
-import getFeatureTooltip from './tooltips/featureTooltip';
-import getStructureTooltip from './tooltips/structureTooltip';
-import getVariationTooltip from './tooltips/variationTooltip';
-
-export {
-  ProtvistaUniprot as default,
-  ProtvistaUniprotStructure,
-  filterConfig,
-  colorConfig,
-  getFeatureTooltip,
-  getStructureTooltip,
-  getVariationTooltip,
-};
+export default ProtvistaUniprot;
