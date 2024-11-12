c51f535417994ecc67c427d9af5e493dbd0f94b3
Daniel Rice <dlrice@ebi.ac.uk>
2024-11-12 13:13:35 +0000

    Build ES modules.

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,36 @@
-const webpack = require('webpack');
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 const PACKAGE_ROOT_PATH = process.cwd();
 
 const config = {
-  entry: ['./src/index.ts'],
+  entry: './src/index.ts',
   output: {
     path: path.resolve(PACKAGE_ROOT_PATH, 'dist'),
     publicPath: '/',
     chunkFilename: '[name].js',
-    library: 'ProtvistaUniprot',
-    filename: 'protvista-uniprot.js',
+    filename: 'protvista-uniprot.mjs',
     clean: true,
+    library: {
+      type: 'module',
+    },
   },
   target: 'web',
   devtool: 'source-map',
   resolve: {
     extensions: ['.ts', '.js'],
   },
-  externals: {
-    d3: 'd3',
-  },
   module: {
     rules: [
       {
         test: /\.(js|ts)$/,
         exclude:
-          /node_modules\/(?!(lit-element|lit-html|timing-functions|protvista-*|data-loader)).*/,
-        use: {
-          loader: 'babel-loader',
-        },
+          /node_modules\/(?!(lit-element|lit-html|timing-functions|protvista-*)).*/,
+        use: 'babel-loader',
       },
       {
         test: /\.svg$/,
-        loader: 'svg-inline-loader',
+        use: 'svg-inline-loader',
       },
       {
         test: /\.css$/i,
@@ -47,8 +43,11 @@ const config = {
       patterns: [{ from: 'src/icons', to: 'es/icons' }],
     }),
   ],
+  experiments: {
+    outputModule: true,
+  },
   devServer: {
-    contentBase: [
+    static: [
       path.join(__dirname, 'public'),
       path.join(__dirname, 'node_modules'),
     ],
